e16a263b869f724751a73ddb7ee8e796c682ef35
lala <lala@mediot.com.tw>
2020-04-30 00:27:11 +0800

    Configured multiple jvbs

--- prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
@@ -154,3 +154,27 @@ Component "lobby.{{ .Env.XMPP_DOMAIN }}" "muc"
     muc_room_locking = false
     muc_room_default_public_jids = true
 {{ end }}
+
+Component "ap-northeast-1-jvb-1.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-2.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-3.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-4.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-5.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-6.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-7.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
+
+Component "ap-northeast-1-jvb-8.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JVB_COMPONENT_SECRET }}"
