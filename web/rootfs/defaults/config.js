d7ea4cd9d550833c27205758b0e11490e8c94447
lala <lala@mediot.com.tw>
2020-07-11 13:17:06 +0800

    Configured the stun servers list

--- web/rootfs/defaults/config.js
+++ web/rootfs/defaults/config.js
@@ -350,6 +350,9 @@ var config = {
         // The STUN servers that will be used in the peer to peer connections
         stunServers: [
           { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' },
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:stun2.l.google.com:19302' }
         ],
 
         // Sets the ICE transport policy for the p2p connection. At the time
